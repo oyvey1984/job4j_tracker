71dfb5472ec25924555d238e3ce8a76b3b85fbbb
oyvey1984 <ayrat.tazetdinov@mail.ru>
2022-12-12 18:08:54 +0500

    1.5. Поля объекта [#166083 #369877]

--- src/main/java/ru/job4j/oop/Cat.java
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -4,18 +4,18 @@ public class Cat {
 
     private String food;
 
-    public void show() {
-        System.out.println(this.name + " " + this.food);
-    }
+    private String name;
 
     public void eat(String meat) {
         this.food = meat;
     }
 
-    private String name;
-
     public void giveNick(String nick) {
         this.name = nick;
+    }
+
+    public void show() {
+        System.out.println(this.name + " " + this.food);
     }
 
     public static void main(String[] args) {
