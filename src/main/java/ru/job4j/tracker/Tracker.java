f11d2a48eca4df5ccfcaec28d9d9357e95c87096
oyvey1984 <ayrat.tazetdinov@mail.ru>
2023-02-21 21:52:40 +0500

    5. Tracker - хранилище [#396 #388224]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -14,16 +14,7 @@ public class Tracker {
     }
 
     public Item[] findAll() {
-        Item[] rsl = new Item[size];
-        int length = 0;
-        for (int index = 0; index < size; index++) {
-            Item rtr = items[index];
-            if (rtr != null) {
-                rsl[length] = rtr;
-                length++;
-            }
-        }
-        return Arrays.copyOf(rsl, length);
+        return Arrays.copyOf(items, size);
     }
 
     public Item[] findByName(String key) {
@@ -39,15 +30,19 @@ public class Tracker {
         return Arrays.copyOf(rsl, length);
     }
 
-    public Item findById(int id) {
-        Item rsl = null;
+    private int indexOf(int id) {
+        int rsl = -1;
         for (int index = 0; index < size; index++) {
-            Item item = items[index];
-            if (item.getId() == id) {
-                rsl = item;
+            if (items[index].getId() == id) {
+                rsl = index;
                 break;
             }
         }
         return rsl;
+    }
+
+    public Item findById(int id) {
+        int index = indexOf(id);
+        return index != -1 ? items[index] : null;
     }
 }
