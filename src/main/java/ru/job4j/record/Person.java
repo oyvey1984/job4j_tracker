65b842d8ad04904aa24349d7a2ad9b361461c71c
oyvey1984 <ayrat.tazetdinov@mail.ru>
2023-03-30 22:31:03 +0500

    исправление файла, на который не ругался чекстайл

--- src/main/java/ru/job4j/record/Person.java
+++ src/main/java/ru/job4j/record/Person.java
@@ -21,8 +21,12 @@ public class Person {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         Person person = (Person) o;
         return age == person.age && Objects.equals(name, person.name);
     }
