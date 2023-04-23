e5d79ee6bb01123cec8215c1fa67cb81fdb82bfc
oyvey1984 <ayrat.tazetdinov@mail.ru>
2023-04-23 20:10:57 +0500

    19. Вывод значения переменной с поясняющей надписью

---
+++ src/main/java/ru/job4j/io/Task19.java
@@ -0,0 +1,9 @@
+package ru.job4j.io;
+
+public class Task19 {
+    public static void main(String[] args) {
+        int x = 9;
+        System.out.println("Значение x равно " + x);
+        System.out.printf("Значение x^2 равно %.0f%n", Math.pow(x, 2));
+    }
+}
