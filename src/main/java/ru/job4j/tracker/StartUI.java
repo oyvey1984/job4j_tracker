c161d8b15f46e9e2f288a9e2c19bab566cf1de3f
oyvey1984 <ayrat.tazetdinov@mail.ru>
2023-06-21 19:08:03 +0500

    3.5. Реализация класса StartUI. Вывод заявки по id [#500748  [#500748 #411665]]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -56,7 +56,7 @@ public class StartUI {
                 if (item != null) {
                     System.out.println(item);
                 } else {
-                    System.out.println("Заявка с введенным id: " + id + " не найдена.");
+                    System.out.print("Заявка с введенным id: " + id + " не найдена.");
                 }
             } else if (select == 5) {
                 System.out.println("=== Find items by name ===");
