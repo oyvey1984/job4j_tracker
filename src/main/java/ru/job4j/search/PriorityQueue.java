4251861511b7c0407c84dcab0cc0fa4c76ad740b
oyvey1984 <ayrat.tazetdinov@mail.ru>
2023-10-10 21:00:15 +0500

    5. Очередь с приоритетом на LinkedList [#41670 #451101]

--- src/main/java/ru/job4j/search/PriorityQueue.java
+++ src/main/java/ru/job4j/search/PriorityQueue.java
@@ -8,12 +8,10 @@ public class PriorityQueue {
     public void put(Task task) {
         int index = 0;
         for (Task element : tasks) {
-            if (task.getPriority() >= element.getPriority()) {
-                index++;
-                break;
-            } else if (task.getPriority() < element.getPriority()) {
+            if (task.getPriority() < element.getPriority()) {
                 break;
             }
+            index++;
         }
         this.tasks.add(index, task);
     }
