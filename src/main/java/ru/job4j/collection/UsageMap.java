a4b145f620b15d223a2b559f9d051f04b1d7982a
oyvey1984 <ayrat.tazetdinov@mail.ru>
2024-02-08 12:41:51 +0500

    0. Map, HashMap [#212652]

---
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -0,0 +1,15 @@
+package ru.job4j.collection;
+
+import java.util.HashMap;
+
+public class UsageMap {
+    public static void main(String[] args) {
+        HashMap<String, String> emailDataBase = new HashMap<>();
+        emailDataBase.put("ayrat.tazetdinov@mail.ru", "Ayrat Tazetdinov");
+        emailDataBase.put("oyvey@gmail.com", "Duhast Vaycheslavov");
+        emailDataBase.put("a.pop@yandex.ru", "Ivan Petrov");
+        for (String key : emailDataBase.keySet()) {
+            System.out.println(emailDataBase.get(key));
+        }
+    }
+}
