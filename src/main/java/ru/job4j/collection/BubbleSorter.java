aa12008369fd2e4167428d97166e43ed6ae56db1
oyvey1984 <ayrat.tazetdinov@mail.ru>
2024-03-06 07:40:25 +0500

    тренировка Пузырьковой сортировки

--- src/main/java/ru/job4j/collection/BubbleSorter.java
+++ src/main/java/ru/job4j/collection/BubbleSorter.java
@@ -5,10 +5,8 @@ import java.util.Arrays;
 public class BubbleSorter {
     public static void sort(int[] array) {
         for (int i = 0; i < array.length - 1; i++) {
-            //внутренний цикл -за перебор элементов в одном проходе
             for (int j = array.length - 1; j > i; j--) {
                 if (array[j - 1] > array[j]) {
-                    //переменная temp отвечает за обмен значений
                     int temp = array[j - 1];
                     array[j - 1] = array[j];
                     array[j] = temp;
@@ -21,7 +19,6 @@ public class BubbleSorter {
     public static void main(String[] args) {
         int[] arr = {8, 7, 4, 1, 2, 9, 3, 5, 6};
         sort(arr);
-
     }
 
     private void swap(int[] array, int ind1, int ind2) {
