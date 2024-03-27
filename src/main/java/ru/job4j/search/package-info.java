8d47bdbce74e2d2f856eb4a52680c09bf6211f59
oyvey1984 <ayrat.tazetdinov@mail.ru>
2024-03-27 19:43:13 +0500

    Примеры из 2. Что такое JavaDoc [#313879]

---
+++ src/main/java/ru/job4j/search/package-info.java
@@ -0,0 +1,9 @@
+package ru.job4j.search;
+
+/**
+ * В этом пакете расположены классы, созданные привыполнении заданя "4. Телефонный справочник на базе ArrayList"
+ * <p> пакет дополнен классом PriorityQueue</>
+ * @since 27.03.2024
+ * @author Ayrat Tazetdinov
+ * @version 1.0
+ */
