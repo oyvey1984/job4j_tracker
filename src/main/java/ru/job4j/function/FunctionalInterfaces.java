04f853c9a6928d75ded8d65d9437b71d180ae2f5
oyvey1984 <ayrat.tazetdinov@mail.ru>
2024-05-26 12:48:25 +0300

    2.2. Функциональный интерфейс [#247497 #496911]

--- src/main/java/ru/job4j/function/FunctionalInterfaces.java
+++ src/main/java/ru/job4j/function/FunctionalInterfaces.java
@@ -16,7 +16,7 @@ public class FunctionalInterfaces {
         biConsumer.accept(7, "seven");
 
         BiPredicate<Integer, String> biPredicate = (first, second) ->
-                (first % 2 == 0 || map.get(first).length() == 4);
+                (first % 2 == 0 || second.length() == 4);
         for (Integer key : map.keySet()) {
             if (biPredicate.test(key, map.get(key))) {
                 System.out.println("key: " + key + " value: " + map.get(key));
