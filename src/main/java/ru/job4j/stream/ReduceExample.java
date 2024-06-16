871e448c051f9e26fe1ed42223d708726f30b7e5
oyvey1984 <ayrat.tazetdinov@mail.ru>
2024-06-16 17:48:54 +0300

    15. Агрегатный метод reduce() [#504929]

---
+++ src/main/java/ru/job4j/stream/ReduceExample.java
@@ -0,0 +1,46 @@
+package ru.job4j.stream;
+
+import ru.job4j.stream.mapto.Person;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+public class ReduceExample {
+    public static void main(String[] args) {
+        List<Integer> numbers = List.of(1, 2, 3, 4);
+        Optional<Integer> sum = numbers.stream()
+                .reduce((left, right) -> left + right);
+        System.out.println(sum.get());
+
+        List<String> nums = List.of("Один", "Два", "Три");
+        Optional<String> sumStr = nums.stream()
+                .reduce((left, right) -> left + " " + right);
+        System.out.println(sumStr.get());
+
+        int sumInt = numbers.stream()
+                .reduce(2, (left, right) -> left + right);
+        System.out.println(sumInt);
+
+        List<Person> people = Arrays.asList(
+                new Person("Михаил", 35),
+                new Person("Ольга", 26),
+                new Person("Антон", 20),
+                new Person("Виктор", 16),
+                new Person("Анна", 29)
+        );
+        int sumAge = people.stream()
+                .reduce(
+                        0,
+                        (left, right) -> {
+                            if (right.getAge() > 25) {
+                                return left + right.getAge();
+                            } else {
+                                return left;
+                            }
+                        },
+                        (left, right) -> left + right
+                );
+        System.out.println(sumAge);
+    }
+}
